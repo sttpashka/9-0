ca5b4b76ab82cbb70f2b052600142a340d87721b
степа <118766257+sttpashka@users.noreply.github.com>
2023-10-25 15:24:57 +0300

    Add files via upload

---
+++ Practica9/README.md
@@ -0,0 +1,19 @@
+# EmailApp #
+
+## Overview
+This project is a simple windows application to send emails. The project is done using C# and WPF under the .NET framework.
+
+Emails and passwords are redacted for security reasons.
+
+## Objective
+Learn and practice C# .NET and WPF.
+
+## Features
+1. Select a recipient from a dropdown menu or enter recipients' address (separated by ";" for multiple recipients)
+2. Enter a email subject(optional)
+3. Attach a file(optional)
+4. Enter a email body(optional)
+
+## Use
+1. Change FROM_EMAIL and FROM_PASS to sending email and its password respectively
+2. Change ComboBox emails
---
+++ Practica9/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net.Mail;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    
+    public partial class MainWindow : Window
+    {
+        private string FROM_EMAIL = "email@gmail.com";
+        private string FROM_PASS = "emailPassword123!";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+                fileName.Text = openFileDialog.FileName;
+        }
+
+        private void btnSend_Click(object sender, RoutedEventArgs e)
+        {
+            
+            SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+            SmtpServer.Port = 587;
+            SmtpServer.Credentials = new System.Net.NetworkCredential(FROM_EMAIL, FROM_PASS);
+            SmtpServer.EnableSsl = true;
+
+            MailMessage mail = new MailMessage();
+            
+            mail.From = new MailAddress(FROM_EMAIL);
+
+            //email recipient
+            string addresses = toEmail.Text;
+            foreach (var address in addresses.Split(new[] { ";" }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                if (address.Contains("@") == false || address.Contains(".com") == false)
+                {
+                    MessageBox.Show("Пожалуйста, введите фактический адрес электронной почты");
+                    return;
+                }
+                mail.To.Add(address);
+            }
+            
+            mail.Subject = subjectContent.Text;
+
+            
+            if (string.IsNullOrEmpty(fileName.Text) == false)
+            {
+                mail.Attachments.Add(new Attachment(fileName.Text));
+            }
+
+            
+            mail.Body = emailContent.Text;
+
+            
+            MessageBox.Show("Отправка...\nПожалуйста, закройте это и дождитесь отправленного подтверждения.");
+            SmtpServer.Send(mail);
+            MessageBox.Show("Отправлено");
+
+            
+            toEmail.Text = "";
+            subjectContent.Text = "";
+            fileName.Text = "";
+            emailContent.Text = "\n\nОтправлено из приложения wpf";
+        }
+    }
+}
